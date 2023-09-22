1b21ab537d60751a3a024673add57559cb8d3f8a
Anthony Ho <anthonyho992@gmail.com>
2023-09-21 18:34:50 -0600

    Assignment 2 done?
    Completed Assignment 2 possibly

---
+++ app/StudentInfo.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Page(){
+    return(
+        <div>
+            <h1>
+                <li>Name: Anthony</li> 
+                <li>CPRG 306 B</li>  
+                <p>
+                    <Link href="https://github.com/AnthoynHo/cprg306-assignments">https://github.com/AnthoynHo/cprg306-assignments</Link>
+                </p> 
+            </h1>
+        </div>
+    )
+}
+
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from 'app/StudentInfo'
+
+export default function Page() {
+    return(
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo/>
+
+        </main>
+    )
+}
+

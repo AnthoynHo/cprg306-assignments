ab34543704f060a0208a5da582e77dfb05ac7329
Anthony Ho <anthonyho992@gmail.com>
2023-10-09 22:31:05 -0600

    Done week2
    validation working? and Stylized

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -13,7 +13,7 @@ export default function NewItem() {
             category: category
         });
         event.preventDefault();
-        if (event.input.required = true){
+        if (name == '' || quantity < 1){
             alert('Please fill out all fields.');
         }
         else {
@@ -28,59 +28,65 @@ export default function NewItem() {
     };
     
     return(
-        <form>
-            <label> {/*label for name*/}
-                Name:
-                <input
-                required 
-                id = "name"
-                className="text-black" 
-                type="text" 
-                placeholder="Enter name of item." 
-                value={name} 
-                onChange={event => setName(event.target.value)}
-                
-                />
-            </label>
-            <label> {/*label for quantity*/}
-                Quantity:
-                <input
-                name = "quantity" 
-                className="text-black" 
-                type="number"
-                min = "1"
-                max = "99"
-                value={quantity}
-                placeholder="Enter quantity."  
-                onChange={event => setQuantity(event.target.value)}
-                required
-                />
-            </label>
-            <label> {/*label for category*/}
-                Category:
-                <select
-                name = "category" 
-                className="text-black" 
-                type="text" 
-                placeholder="Enter category." 
-                onChange={event => setCategory(event.target.value)}
-                >
-                    <option value="Produce">Produce</option>
-                    <option value="Dairy">Dairy</option>
-                    <option value="Bakery">Bakery</option>
-                    <option value="Meat">Meat</option>
-                    <option value="Frozen Foods">Frozen Foods</option>
-                    <option value="Canned Goods">Canned Goods</option>
-                    <option value="Dry Goods">Dry Goods</option>
-                    <option value="Beverages">Beverages</option>
-                    <option value="Snacks">Snacks</option>
-                    <option value="Household">Household</option>
-                    <option value="Other">Other</option>
-                    </select>
-            </label>
-            <label>
-                <input type="submit" value="Add item to list" onClick={handleSubmit} />
-            </label>
+        <form className="flex justify-center">
+            <div className="grid-cols-2 place-items-center">
+                <div className="col-span-2 justify-center m-8 flex">
+                    <label className="grow-1 p-4"> {/*label for name*/}
+                        <input
+                        required 
+                        id = "name"
+                        className="text-white p-2 bg-purple-700 border-purple-700 border-8 rounded" 
+                        type="text" 
+                        placeholder="Enter name of item." 
+                        value={name} 
+                        onChange={event => setName(event.target.value)}                
+                        />
+                    </label>
+                </div>
+                <div className="flex">
+                    <label className="p-4"> {/*label for quantity*/}
+                        <input
+                        name = "quantity" 
+                        className="text-white p-2 border-purple-700 border-8 rounded bg-purple-700" 
+                        type="number"
+                        min = "1"
+                        max = "99"
+                        value={quantity}
+                        placeholder="Enter quantity."
+                        flex-grow="1"  
+                        onChange={event => setQuantity(event.target.value)}
+                        required
+                        />
+                    </label>
+                    <label className="p-4"> {/*label for category*/}
+                        <select
+                        name = "category" 
+                        className="text-white p-2 bg-purple-700 border-purple-700 border-8 rounded" 
+                        type="text" 
+                        placeholder="Enter category."
+                        flex-grow="1" 
+                        onChange={event => setCategory(event.target.value)}
+                        >
+                            <option value="Produce">Produce</option>
+                            <option value="Dairy">Dairy</option>
+                            <option value="Bakery">Bakery</option>
+                            <option value="Meat">Meat</option>
+                            <option value="Frozen Foods">Frozen Foods</option>
+                            <option value="Canned Goods">Canned Goods</option>
+                            <option value="Dry Goods">Dry Goods</option>
+                            <option value="Beverages">Beverages</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Household">Household</option>
+                            <option value="Other">Other</option>
+                            </select>
+                    </label>
+                </div>
+                <div className="p-5 m-8 flex justify-center">
+                    <label className="p-8 rounded border-8 border-cyan-700 bg-red-700">
+                        <input type="submit" value="Add item to list" onClick={handleSubmit} />
+                    </label>
+                </div>
+            </div>
         </form>
     )
 }
